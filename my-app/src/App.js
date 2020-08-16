d051b16c56e094f6ed8c4e11323f6260f4709d3d
Tyler <0tylernguyen@gmail.com>
2020-08-15 22:18:02 -0700

    Barebones page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './maskemoji.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Face Mask Detection
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <header className="App-body">
+        <p>
+          Wearing a mask is a deemed as a way to slow the spread of virus amid the Covid-19 pandemic.
+          Our team trained a model that should detect whether the person in the image has face mask on or off. The model can be used in various ways:
+        </p>
+        <ul>
+          <li > For grocery stores where face masks are required for entry. </li>
+          <li> To detect if a person at a door is wearing a mask before you open it.</li>
+
+        </ul>
+        <button onClick={upload}>Upload your image</button>
       </header>
     </div>
+    
   );
+}
+
+function upload() {
+  alert("do something");
 }
 
 export default App;
