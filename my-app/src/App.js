fee175b5536a75620da48cef96c521a20dcc33b6
Tyler <0tylernguyen@gmail.com>
2020-08-16 11:00:11 -0700

    :(

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './maskemoji.png';
 import './App.css';
-const tf = require('@tensorflow/tfjs');
+import model from './model.json';
+const mod = model;
 
+const tf = require('@tensorflow/tfjs');
 
 
 function App() {
@@ -32,6 +34,9 @@ function App() {
             You can upload an image here and we'll detect the presence of masks. While this may be a trivial application for our model, it serves the purpose of demonstrating
             the accuracy and concept of our project.
           </p>
+          <p style={{color: 'yellow', fontSize: 35}}>
+            Upload your image here:
+          </p>
           
           <ImageForm> </ImageForm>
 
@@ -50,51 +55,50 @@ function App() {
 
 
 class ImageForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {selectedFile: null};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    
-  }
+  
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} method="POST" enctype="multipart/form-data">
-        <p style={{fontSize: 40, color: 'yellow'}}>Upload your image:</p>
-        <input type="file" multiple="false" accept="image/*" name="file" onChange={this.handleChange}></input>
-        <input type="submit" name="submit" value="Submit"></input>
-      </form>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ selectedFile: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    return (
+      /*
       <div>
-        <img id="image" crossorigin="anonymous" src={this.state.selectedFile} alt="This is not an image"/>
-        {this.run()}
+        <input type="file" onChange="readURL(this)"></input>
+        <button onClick={this.run()}>Upload</button>
+        <img id="image" crossorigin="anonymous" alt="your image"></img>
+      </div>
+      */
+      <div>
+        <input type="file"></input>
+        <input type="submit" onClick={cry}></input>
       </div>
     );
   }
 
+  /*
+  readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        ("#image").attr("src", e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
   async run() {
-    const model = await tf.automl.loadImageClassification('model.json');
+    const model = await tf.automl.loadImageClassification(mod);
     const image = document.getElementById('image');
     const predictions = await model.classify(image);
-
     console.log(predictions);
-
     // Show the resulting object on the page.
     const pre = document.createElement('pre');
     pre.textContent = JSON.stringify(predictions, null, 2);
     document.body.append(pre);
   }
+  */
 
+}
+
+function cry() {
+  alert(":(");
 }
 
 
